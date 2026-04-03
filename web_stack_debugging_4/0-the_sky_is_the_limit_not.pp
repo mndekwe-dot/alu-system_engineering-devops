e6595e6481c1f6802e0e2508507e7427938a3d70
mndekwe-dot <m.ndekwe@alustudent.com>
2026-04-03 13:00:07 +0200

    Fix ULIMIT sed to match any existing value

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # Fixes Nginx failed requests by increasing the ULIMIT of open files
 
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/ULIMIT=.*/ULIMIT=\"-n 4096\"/" /etc/default/nginx && nginx -s reload',
-  path    => '/usr/local/bin/:/bin/:/usr/bin/:/usr/sbin/',
+  command => 'sed -i "s/ULIMIT=.*/ULIMIT=\"-n 4096\"/" /etc/default/nginx && service nginx restart',
+  path    => '/usr/local/bin/:/bin/:/usr/bin/:/usr/sbin/:/sbin/',
 }
