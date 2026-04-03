e0b5e8364d7002fbc2cb5224e5baa51b67bd473d
mndekwe-dot <m.ndekwe@alustudent.com>
2026-04-03 12:44:21 +0200

    Fix WordPress 500 error using Puppet - correct phpp typo

---
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fixes WordPress 500 error by correcting the .phpp typo to .php in wp-settings.php
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/:/usr/bin/',
+}
